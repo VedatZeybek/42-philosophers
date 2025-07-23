ce6bd9fdc470fff4767940e36864fcf4dd78e3f3
vedat.zeybek <vedatzeybek20@gmail.com>
2025-07-23 19:52:53 +0300

    philosophers thinking about the die

--- main.c
+++ main.c
@@ -1,36 +1,38 @@
 #include "philosophers.h"
 
-int	control_args(int argc, char **argv)
-{
-	int	i;
-	int	j;
+// int	control_args(int argc, char **argv)
+// {
+// 	int	i;
+// 	int	j;
 
-	if (argc < 5 || argc > 6 || ft_atoi(argv[1]) > 20)
-		return (0);
-	i = 0;
-	while (argv[i])
-	{
-		j = 0;
-		while (argv[i][j])
-		{
-			if (argv[i][j] < '0' || argv[i][j] > '9')
-				return (0);
-			j++;
-		}
-		i++;
-	}
-}
+// 	if (argc < 5 || argc > 6 || ft_atoi(argv[1]) > 20)
+// 		return (0);
+// 	i = 0;
+// 	while (argv[i])
+// 	{
+// 		j = 0;
+// 		while (argv[i][j])
+// 		{
+// 			if (argv[i][j] < '0' || argv[i][j] > '9')
+// 				return (0);
+// 			j++;
+// 		}
+// 		i++;
+// 	}
+// }
 
+struct s_table;
+typedef struct s_table t_table;
 
 typedef struct s_philo
 {
-	int		philo_id;
-	int		*data;
-	int		left_fork;
-	int		right_fork;
-	t_table	*table;
+	int			philo_id;
+	int			*data;
+	int			left_fork;
+	int			right_fork;
+	t_table		*table;
+	pthread_t	thread;
 }	t_philo;
-
 
 typedef struct s_table
 {
@@ -42,7 +44,6 @@ typedef struct s_table
 }	t_table;
 
 
-
 int	get_philo_count(char **argv)
 {
 	int	count;
@@ -51,15 +52,15 @@ int	get_philo_count(char **argv)
 	return (count);
 }
 
-void	fill_stats_table(t_table *table, int count)
+t_table	*fill_stats_table(int count)
 {
-	int	i;
+	t_table	*table;
+	int		i;
 	
 	i = 0;
 	table = malloc(sizeof(t_table));
-	
 	table->philo = malloc (sizeof(t_philo *) * count);
-	table->forks = malloc(sizeof(int *));
+	table->forks = malloc(sizeof(int) * count);
 	while (i < count)
 	{
 		table->philo[i] = malloc(sizeof(t_philo));
@@ -70,19 +71,21 @@ void	fill_stats_table(t_table *table, int count)
 		table->forks[i] = i;
 		i++;
 	}
+	return (table);
 }
 
 void	*philo_function(void* arg) 
 {
 	t_philo	*philo = (t_philo *)arg;
+	
 
 	while (1)
 	{
 		printf("Philo %d has taken a fork...\n", philo->philo_id);
-		
+
 		printf("Philo %d is eating...\n", philo->philo_id);
 		sleep(2);
-
+		
 		printf("Philo %d is thinking...\n", philo->philo_id);
 		sleep(2);
 		printf("Philo %d is sleeping.. \n", philo->philo_id);
@@ -94,30 +97,33 @@ int	main(int argc, char **argv)
 {
 	
 	int		philo_count;
-	pthread_t thread1;
-	pthread_t thread2;
-	int		id_1 = 1;
-	int		id_2 = 2;
+	int		i;
 	t_table	*table;
 
+	
 	philo_count = get_philo_count(argv);
 	if (philo_count < 2 || philo_count > 20)
-		return (0);
-
-	fill_stats_table(table, philo_count);
+	return (0);
 	
-	
-	pthread_create(&thread1, NULL, philo_function, &table);
-	pthread_create(&thread2, NULL, philo_function, &table);
+	table = fill_stats_table(philo_count);
+	i = 0;
+	while (i < philo_count)
+	{
+		pthread_create(&(table->philo[i]->thread), NULL, philo_function, table->philo[i]);
+		i++;
+	}
+	i = 0;
+	while (i < philo_count)
+	{
+		pthread_join(table->philo[i]->thread, NULL);
+		i++;
+	}
 
 	//pthread_detach(thread1);
     //printf("Thread1 detached - main won't wait\n");
-    
     // 3. Thread2'yi join et
     // printf("Waiting for Thread2...\n");
-    pthread_join(thread2, NULL);
-    //printf("Thread2 result: %ld\n", (long)result);
+	//printf("Thread2 result: %ld\n", (long)result);
     //sleep(1);
-    
     //printf("Main thread finished\n");
 }
