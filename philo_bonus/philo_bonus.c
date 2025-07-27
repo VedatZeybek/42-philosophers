939c660992f12488dbc4b29b765c649f960e1691
vedat.zeybek <vedatzeybek20@gmail.com>
2025-07-27 19:44:51 +0300

    generic philo process created

--- philo_bonus/philo_bonus.c
+++ philo_bonus/philo_bonus.c
@@ -1,82 +1,51 @@
 #include "philosophers_bonus.h"
 
-static int	one_philo(t_philo *philo)
-{
-	if (philo->table->philo_count == 1)
-	{
-		sem_wait(philo->table->forks);
-		printf("%d has taken a fork", philo->philo_id);
-		usleep(philo->table->time_to_die * 1000);
-		sem_post(philo->table->forks);
-		return (1);
-	}
-	return (0);
-}
-
-static int	is_dead(t_philo *philo, long last_eat)
-{
-	long	current_time;
-
-	current_time = get_timestamp(philo->table);
-	if (current_time - last_eat > philo->table->time_to_die)
-	{
-		printf("%ld %d died\n", current_time, philo->philo_id);
-		return (1);
-	}
-	return (0);
-}
-
-void	philo_process(t_philo *philo)
-{
-	int i = 0;
-
-	if (one_philo(philo))
-		return ;
-	while ((philo->table->cycle_count == -1 || i < philo->table->cycle_count) 
-		   && !philo->table->simulation_end)
-	{
-		sem_wait(philo->table->forks);
-		printf("%ld %d has taken a fork\n", get_timestamp(philo->table), philo->philo_id);
-        
-		sem_wait(philo->table->forks);
-		printf("%ld %d has taken a fork\n", get_timestamp(philo->table), philo->philo_id);
-        printf("%ld %d %s\n", get_timestamp(philo->table), philo->philo_id, "is eating.");
-		usleep(philo->table->time_to_eat * 1000);
-		sem_post(philo->table->forks);
-		sem_post(philo->table->forks);
-
-        printf("%ld %d %s\n", get_timestamp(philo->table), philo->philo_id, "is sleeping.");
-		usleep(philo->table->time_to_sleep * 1000); 
-        printf("%ld %d %s\n", get_timestamp(philo->table), philo->philo_id, "is thinking."); 
-		i++;
-	}
-}
-
 int	main(int argc, char **argv)
 {
- 
-	t_table *table;
+	t_table	*table;
 	int		count;
 	int		i;
+	pid_t	*pids;
+	pid_t	dead_pid;
 
 	i = 0;
 	count = get_philo_count(argv);
 	table = fill_table_stats(count, argv);
+	pids = malloc(sizeof(pid_t) * count);
+	if (!pids)
+		return (1);
 	while (i < count)
 	{
-		pid_t pid0 = fork();
-		if (pid0 == 0)
+		pids[i] = fork();
+		if (pids[i] == 0)
 		{
 			philo_process(table->philo[i]);
-			exit(EXIT_SUCCESS);
+			exit(0);
+		}
+		else if (pids[i] < 0)
+		{
+			printf("Fork failed\n");
+			return (1);
 		}
 		i++;
 	}
-	
+	dead_pid = waitpid(-1, NULL, 0);
+	i = 0;
+	while (i < count)
+	{
+		if (pids[i] != dead_pid)
+			kill(pids[i], 9);
+		i++;
+	}
+	i = 0;
+	while (i < count)
+	{
+		if (pids[i] != dead_pid)
+			waitpid(pids[i], NULL, 0);
+		i++;
+	}
 	sem_close(table->forks);
-    sem_unlink("/forks");
-
-	wait(NULL);
-	wait(NULL);
-	wait(NULL);
+	sem_unlink("/forks");
+	free(pids);
+	return (0);
 }
---
+++ philo_bonus/philo_process.c
@@ -0,0 +1,88 @@
+#include "philosophers_bonus.h"
+
+static int	one_philo(t_philo *philo)
+{
+	if (philo->table->philo_count == 1)
+	{
+		sem_wait(philo->table->forks);
+		printf("%d has taken a fork", philo->philo_id);
+		usleep(philo->table->time_to_die * 1000);
+		sem_post(philo->table->forks);
+		return (1);
+	}
+	return (0);
+}
+
+static int	is_dead(t_philo *philo, long last_eat)
+{
+	long	current_time;
+
+	current_time = get_timestamp(philo->table);
+	if (current_time - last_eat > philo->table->time_to_die)
+	{
+		printf("%ld %d died\n", current_time, philo->philo_id);
+		return (1);
+	}
+	return (0);
+}
+
+static int	safe_sleep(t_philo *philo, long sleep_duration, long last_meal_time)
+{
+	long sleep_start;
+	long elapsed;
+
+	sleep_start = get_timestamp(philo->table);
+	while (1)
+	{
+		if (is_dead(philo, last_meal_time))
+			return (1);
+		
+		elapsed = get_timestamp(philo->table) - sleep_start;
+		if (elapsed >= sleep_duration)
+			break;
+		usleep(1000);
+	}
+	return (0);
+}
+
+void	philo_process(t_philo *philo)
+{
+	int		i;
+	long	last_meal_time;
+
+	i = 0;
+	if (one_philo(philo))
+		return ;
+	
+	last_meal_time = get_timestamp(philo->table);
+	
+	while (philo->table->cycle_count == -1 || i < philo->table->cycle_count)
+	{
+		if (is_dead(philo, last_meal_time))
+			return ;
+		sem_wait(philo->table->forks);
+		printf("%ld %d has taken a fork\n", get_timestamp(philo->table), philo->philo_id);
+		if (is_dead(philo, last_meal_time))
+		{
+			sem_post(philo->table->forks);
+			return ;
+		}
+		sem_wait(philo->table->forks);
+		printf("%ld %d has taken a fork\n", get_timestamp(philo->table), philo->philo_id);
+		last_meal_time = get_timestamp(philo->table);
+		printf("%ld %d is eating\n", get_timestamp(philo->table), philo->philo_id);
+		if (safe_sleep(philo, philo->table->time_to_eat, last_meal_time))
+		{
+			sem_post(philo->table->forks);
+			sem_post(philo->table->forks);
+			return ;
+		}
+		sem_post(philo->table->forks);
+		sem_post(philo->table->forks);
+		printf("%ld %d is sleeping\n", get_timestamp(philo->table), philo->philo_id);
+		if (safe_sleep(philo, philo->table->time_to_sleep, last_meal_time))
+			return ;
+		printf("%ld %d is thinking\n", get_timestamp(philo->table), philo->philo_id);
+		i++;
+	}
+}
